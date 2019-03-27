486b8c41b25bd269f7b8f289d0f5b0bb2a8567ea
trovato.jcp <trovato.jcp@gmail.com>
2019-03-27 16:43:10 +0100

    Ajout de la fonction Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = setTimeout(function(){
+    alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+    if (confirm('Veux-tu aller sur xxxvidsxxx?')) {
+    document.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+} else {
+    // Do nothing!
+}
+}, 10000);
+
